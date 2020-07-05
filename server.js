db0c8f69c52b9e2deab7d050322b8c1c6b8f62e5
unknown <cesarnagamine@gmail.com>
2020-07-05 02:36:07 -0500

    goood

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.use(cors());
 
 
 //ADDED FOR DEPLOYMENT:
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, './build')))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'))
+    res.sendFile(path.join(__dirname, './build'))
 })
 
 

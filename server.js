671fc1f171b728fb8c9d51f15ea4dcb46caf9dc0
unknown <cesarnagamine@gmail.com>
2020-07-07 02:10:18 -0500

    initial scripts and new middleware

--- server.js
+++ server.js
@@ -18,11 +18,8 @@ app.use(cors());
 
 //ADDED FOR DEPLOYMENT:
 
-const publicPath = path.join(__dirname, 'public');
-app.use(express.static(publicPath));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
